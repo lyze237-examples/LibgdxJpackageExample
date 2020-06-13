21d5d770e2867950b76fd74916ec761a1cb198cc
lyze237 <11274700+lyze237@users.noreply.github.com>
2020-06-13 18:48:15 +0200

    init

---
+++ core/src/main/java/dev/lyze/gdxJpackage/GdxJpackage.java
@@ -0,0 +1,7 @@
+package dev.lyze.gdxJpackage;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class GdxJpackage extends ApplicationAdapter {
+}
